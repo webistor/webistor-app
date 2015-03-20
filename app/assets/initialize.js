f129f10ef20f1b6d6940407978c1a03aac25afe5
bartwr <bart@tuxion.nl>
2015-03-20 15:48:15 +0100

    Moved Piwik tracking code to initialize.js

--- app/assets/initialize.js
+++ app/assets/initialize.js
@@ -1,1 +1,17 @@
+
+// Init
 require('initialize');
+
+// Piwik analytics
+var _paq = _paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.webistor.net"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//analytics.tuxion.nl/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 3]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
